b8c99e1b5eeef771da111b84312c83c810093f8f
Goobus-source <aousynth@gmail.com>
2024-11-25 16:41:46 +1030

    added sources and images

---
+++ bookmarks.js
@@ -0,0 +1,43 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+// This is where to add a bunch of bookmarks for easy access..
+// Use the example folder as a template and for understanding
+
+
+const bookmarks = [
+  { 
+    title: "School",
+    links: [
+      { name: "Seqta", url: "https://student.whs.sa.edu.au/" },
+      { name: "Clickview", url: "https://www.clickvieweducation.com/en-au/" },
+      { name: "Blooket", url: "https://id.blooket.com/login" },
+	  { name: "Gimkit", url: "https://www.gimkit.com/join" },
+	  { name: "Kahoot", url: "https://kahoot.it/" },
+    ],
+   },
+  
+  
+  {
+    title: "Daily",
+    links: [
+      { name: "Outlook", url: "https://outlook.com" },
+      { name: "Gmail", url:"https://mail.google.com/mail/"},
+      { name: "Onedrive", url: "https://onedrive.live.com/" },
+      { name: "Google Drive", url: "https://drive.google.com" },
+    ],
+  },
+
+// ---- copy here ----
+// (start of bookmark folder)
+  {
+	// Main contents to add to (you can add as many links as you want)
+    title: "Example Folder",
+    links: [
+      { name: "Example Bookmark", url: "https://www.example.com" },
+	  { name: "To add to this just go to the folder that your running this from and open the bookmarks.js with notepad", url: "https://www.example.com" },
+    ],
+	// -- End Content ---
+  },
+// --- end here ---
+
+
+];
